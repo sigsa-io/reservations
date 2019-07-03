8cd3e1e38d8d964c3860438fbf8be664803c925a
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-03 15:17:45 -0700

    webpack and babel configured

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client', 'index.js'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+      rules: [
+        { 
+            test:  /\.(js||jsx)/, 
+            exclude: /node_modules/, 
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"]
+            },
+        }
+      ]
+  },
+  mode: "development",
+};
