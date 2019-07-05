2727768becd2c7321d9bf23fe51a8da845464762
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-05 15:21:10 -0700

    Date component skeleton and css set up

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import PartySize from './PartySize.jsx';
 import Time from './Time.jsx';
+import Date from './Date.jsx';
 
 class App extends React.Component {
-    constructor() {
-        super();
-        
-    }
+  constructor() {
+    super();
+      
+  }
 
-    render() {
-        return (
-            <div className="reservation-frame">
-                <div className="reservation-title-wrapper">
-                    <h3 className="reservation-title">
-                        <span>Make a reservation</span>
-                    </h3>
-                </div>
-                <div className="reservation-detail-wrapper">
-                    {/* will need to insert the scrolling shrink tag here */}
-                    <div className="reservation-detail-selection">
-                        <div>
-                            <h4 className="reservation-detail-title">Party Size</h4>
-                            <PartySize />
-                        </div>
-                        <div>
-                            <h4 className="reservation-detail-title">Time</h4>
-                            <Time />
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="reservation-frame">
+        <div className="reservation-title-wrapper">
+          <h3 className="reservation-title">
+            <span>Make a reservation</span>
+          </h3>
+        </div>
+        <div className="reservation-detail-wrapper">
+          {/* will need to insert the scrolling shrink tag here */}
+          <div className="reservation-detail-selection">
+            <div>
+              <h4 className="reservation-detail-title">Party Size</h4>
+              <PartySize />
             </div>
-        )
-    }
-    
+            <div className="reservation-detail-date-time-wrapper">
+              <div className="reservation-detail-date-wrapper">
+                <h4 className="reservation-detail-title">Date</h4>
+                <Date />
+              </div>
+              <div className="reservation-detail-time-wrapper">
+                <h4 className="reservation-detail-title">Time</h4>
+                <Time />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
 };
 
 export default App;
