5f844de0ab742263cfad13a8330cf92e4197e520
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-05 16:24:10 -0700

    Partysize file refactor

--- client/components/PartySize.jsx
+++ client/components/PartySize.jsx
@@ -38,11 +38,11 @@ class PartySize extends React.Component {
     const { selectionChange, generatePartySize } = this;
 
     return (
-      <div className="party-size-seleciton-wrapper">
-        <div className="party-size-selection-text">For {partySize}</div>
+      <div className="reservation-seleciton-wrapper">
+        <div className="reservation-selection-text">For {partySize}</div>
         <SVG />
         <select 
-          className="party-size-seleciton"
+          className="reservation-seleciton"
           onChange={selectionChange}
         >
           { generatePartySize() }
