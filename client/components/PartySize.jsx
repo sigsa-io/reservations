83bab773eb0be2b54ce10388881be40e1c46c9ce
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-05 15:04:49 -0700

    added svg icon to selection

--- client/components/PartySize.jsx
+++ client/components/PartySize.jsx
@@ -1,53 +1,55 @@
 import React from 'react';
+import SVG from '../img/SelectionIcon.jsx';
 
 class PartySize extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            partySize: 2,
-        }
-
-        this.generatePartySize = this.generatePartySize.bind(this);
-        this.selectionChange = this.selectionChange.bind(this);
+    this.state = {
+      partySize: 2,
     }
 
-    generatePartySize() {
-        let sizeEntry = [];
+    this.generatePartySize = this.generatePartySize.bind(this);
+    this.selectionChange = this.selectionChange.bind(this);
+  }
 
-        for (let i = 2; i < 21; i ++) {
-            sizeEntry.push(
-                <option 
-                    key={`size-${i}`} 
-                    value={i}
-                >
-                    {i}
-                </option>
-            );
-        }
-        return sizeEntry;
+  generatePartySize() {
+    let sizeEntry = [];
+
+    for (let i = 2; i < 21; i ++) {
+      sizeEntry.push(
+        <option 
+          key={`size-${i}`} 
+          value={i}
+        >
+          {i}
+        </option>
+      );
     }
+    return sizeEntry;
+  }
 
-    selectionChange(e) {
-        this.setState({partySize: e.target.value});
-    }
+  selectionChange(e) {
+    this.setState({partySize: e.target.value});
+  }
 
-    render() {
-        const { partySize } = this.state;
-        const { selectionChange, generatePartySize } = this;
+  render() {
+    const { partySize } = this.state;
+    const { selectionChange, generatePartySize } = this;
 
-        return (
-            <div className="party-size-seleciton-wrapper">
-                <div>For {partySize}</div>
-                <select 
-                    className="party-size-seleciton"
-                    onChange={selectionChange}
-                >
-                    { generatePartySize() }
-                </select>
-            </div>
-        );
-    }
+    return (
+      <div className="party-size-seleciton-wrapper">
+        <div className="party-size-selection-text">For {partySize}</div>
+        <SVG />
+        <select 
+          className="party-size-seleciton"
+          onChange={selectionChange}
+        >
+          { generatePartySize() }
+        </select>
+      </div>
+    );
+  }
 };
 
 export default PartySize;
