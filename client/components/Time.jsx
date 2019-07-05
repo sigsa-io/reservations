11a9999487d226915c52ca58cbfe02a98d66a7b9
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-05 16:24:41 -0700

    time css refactor

--- client/components/Time.jsx
+++ client/components/Time.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
 import moment from 'moment';
+import SVG from '../img/SelectionIcon.jsx';
 
-const Time = () => {
+class Time extends React.Component {
+  constructor() {
+    super();
 
-    const timeGenerator = () => {
-        let timeEntry = [];
-        let hourCount = 48;
-        let time = moment().startOf('day');
+    this.state = {
+      selectedTime: '6:30 PM',
+    }
 
-        while (hourCount > 0) {
-            timeEntry.push(
-                <option 
-                    key={`${time.format('HH')}-${time.format('mm')}`} 
-                    name={`${time.format('HH')}-${time.format('mm')}`}
-                >
-                    {time.format('hh:mm A')}
-                </option>
-            );
-            
-            time.add(30, 'minute');
-            hourCount --;
-        }
+    this.selectionChange = this.selectionChange.bind(this);
+    this.timeGenerator = this.timeGenerator.bind(this);
+  }
 
-        return timeEntry;
-    };
+  timeGenerator () {
+    let timeEntry = [];
+    let hourCount = 48;
+    let time = moment().startOf('day');
+
+    while (hourCount > 0) {
+      timeEntry.push(
+        <option 
+          key={`${time.format('HH')}-${time.format('mm')}`}
+          name={`${time.format('HH')}-${time.format('mm')}`}
+          value={time.format('h:mm A')}
+        >
+          {time.format('hh:mm A')}
+        </option>
+      );
+      
+      time.add(30, 'minute');
+      hourCount --;
+    }
+    return timeEntry;
+  };
+
+  selectionChange(e) {
+    this.setState({
+      selectedTime: e.target.value
+    });
+  }
+
+  render() {
+    const { timeGenerator, selectionChange } = this;
+    const { selectedTime } = this.state;
 
     return (
-        <select>
-            {timeGenerator()}
+      <div className="reservation-seleciton-wrapper">
+        <div className="reservation-selection-text">{ selectedTime }</div>
+        <SVG />
+        <select className="reservation-seleciton" onChange={selectionChange}>
+          { timeGenerator() }
         </select>
-    );
+      </div>
+    )
+  }
+
 };
 
 export default Time;
