43f4c1ade2857fb0bd6af0c9b1faa768e92dfb5c
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-05 23:43:01 -0700

    express.json() implemented

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const router = require('./Route.js');
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.set('port', 3003);
 
