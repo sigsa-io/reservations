5affe44fcee2e5fd28578807796f4f0b6683f70d
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-07 00:38:05 -0700

    static calendar render with basic css

---
+++ client/components/Calendar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import moment from 'moment';
+import CalendarButton from '../img/CalendarButton';
+import CalendarDays from './CalendarDays';
+import CalendarDates from './CalendarDates';
+
+class Calendar extends React.Component {
+  constructor() {
+    super ();
+    this.state = {
+      year: moment().format('YYYY'),
+      month: moment().format('MMMM'),
+      done: false,
+    }
+  }
+
+  render() {
+    const { month, year } = this.state;
+
+    return ( 
+      <div className="calendar-container">
+        <div className="calendar-inner-wrapper">
+          <div className="calendar-button-wrapper">
+            <CalendarButton className='calendar-button calendar-left-button'/>
+            <CalendarButton className='calendar-button calendar-right-button'/>
+          </div>
+          <div className="calendar-detail-wrapper">
+            <div className="calendar-month">
+              <span>{month} {year}</span>
+            </div>
+            <CalendarDays />
+            <CalendarDates 
+              month={month}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Calendar;
+
+
