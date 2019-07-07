3443a0b05da76b127590f7721e6fcec20e97c942
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-06 19:20:02 -0700

    Calendar Days row template created

---
+++ client/components/CalendarDays.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CalendarDays = () => (
+  <tr>
+    <th>Sun</th>
+    <th>Mon</th>
+    <th>Tue</th>
+    <th>Wed</th>
+    <th>Thu</th>
+    <th>Fri</th>
+    <th>Sat</th>
+  </tr>
+)
+
+export default CalendarDays;
