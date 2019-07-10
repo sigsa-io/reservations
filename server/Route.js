d6c7db49de9a3d7a83775a71df4c316be1a8bd51
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-10 16:40:03 -0700

    booking count feature server side implemented

--- server/Route.js
+++ server/Route.js
@@ -27,10 +27,47 @@ router.get('/targettimeslots/:restaurant_id', (req, res) => {
   });
 });
 
-router.get('/datetimeslots/:restaurant_id', (req, res) => {
-  const { restaurant_id } = req.params;
-  const { targetDateStartUnix } = req.query;
+// get available time slots for target date
+router.get('/datetimeslots/:restaurantId', (req, res) => {
+  const { restaurantId } = req.params;
+  const { targetDateStartUnix } = req.body;
   const targetDateEndUnix = Number(targetDateStartUnix) + 60 * 60 * 24;
+
+  // find reservations within this unix range, partysize doesn't matter
+  const queryStrReservations =
+    'SELECT * FROM reservations WHERE restaurantId = ? ' +
+    'AND (start_time BETWEEN ? AND ?)';
+  const queryArgReservations = [restaurantId, targetDateStartUnix, targetDateEndUnix];
+  return db.query(queryStrReservations, queryArgReservations, (err, data) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      // test if data is empty
+      let queryStrRestaurant;
+      let queryArgRestaurant;
+
+      if (data.length === 0) {
+        // we then provide the count of time slots from restaurant table
+        queryStrRestaurant = 'SELECT COUNT(*) FROM restaurants WHERE restaurantId = ?';
+        queryArgRestaurant = [ restaurantId ];
+
+        db.query(queryStrRestaurant, queryArgRestaurant, (err, data) => {
+          if (err) {
+            res.status(500).json(err);
+          } else {
+            console.log(data);
+            res.status(200).json(data);
+          }
+        })
+      } else {
+        // we first get the reservation in 'AMPM' format but in number
+
+      }
+    }
+  })
+
+
+
 
   const queryStr =
     'SELECT * FROM reservations_tables WHERE restaurant_id = ? ' +
@@ -46,18 +83,29 @@ router.get('/datetimeslots/:restaurant_id', (req, res) => {
   });
 });
 
+// create reservation when a time slot is clicked
 router.post('/reservations', (req, res) => {
   const { restaurantId, targetStartTimeUnix, partySize } = req.body;
   const targetEndTimeUnix = Number(targetStartTimeUnix) + 60 * 60;
 
-  const queryStr = 'INSERT INTO reservations (restaurantId, start_time, end_time, partySize) VALUES (?, ?, ?, ?)';
-  const queryArg = [restaurantId, targetStartTimeUnix, targetEndTimeUnix, partySize];
+  const queryStrReservation = 'INSERT INTO reservations (restaurantId, start_time, end_time, partySize) VALUES (?, ?, ?, ?)';
+  const queryArgReservation = [restaurantId, targetStartTimeUnix, targetEndTimeUnix, partySize];
+  const queryStrBookingCount = 'INSERT INTO bookingCount (restaurantId, bookingCount) VALUES (?, 1) ON DUPLICATE KEY UPDATE bookingCount = VALUES(bookingCount) + 1';
+  const queryArgBookingCount = [restaurantId];
 
-  return db.query(queryStr, queryArg, (err, data) => {
+  return db.query(queryStrReservation, queryArgReservation, (err, data) => {
     if (err) {
       res.status(500).json(err);
     } else {
-      res.status(201).json(data);
+      // increase booking count
+      db.query(queryStrBookingCount, queryArgBookingCount, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json(err);
+        } else {
+          res.status(201).json(data);
+        }
+      });
     }
   });
 });
