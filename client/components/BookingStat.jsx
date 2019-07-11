ce912e5680a79224eaed1d92a4ecb30b5ee46575
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-10 18:42:30 -0700

    Booking Counter render updated

--- client/components/BookingStat.jsx
+++ client/components/BookingStat.jsx
@@ -3,55 +3,42 @@ import propTypes from 'prop-types';
 import BookedTimes from '../img/BookTimes';
 import ActFast from '../img/ActFast';
 
-class BookingStat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookedTimes: 0,
-    };
-  }
-
-  render() {
-    const { bookedTimes } = this.state;
-    const { availableDateTimeSlots } = this.props;
-
-    return (
-      <div className="book-stat">
-        <div className="book-stat-wrapper">
-          <BookedTimes
-            className="book-times-icon"
-            viewBox="0 0 24 24"
-            fill="#333333"
-          />
-          <div className="restaurant-booking-stat-text">
-                Booked
-            {' '}
-            {bookedTimes}
-            {' '}
-                times today
-          </div>
-        </div>
-        <div className="book-stat-wrapper">
-          <ActFast
-            className="act-fast-icon"
-            viewBox="0 0 24 24"
-            fill="#333333"
-          />
-          <div className="restaurant-booking-stat-text">
-            {'You\'re in luck! We still have'}
-            {' '}
-            {availableDateTimeSlots}
-            {' '}
-                timeslots left
-          </div>
-        </div>
+const BookingStat = ({ bookingCount, availableTargetTimeSlots }) => (
+  <div className="book-stat">
+    <div className="book-stat-wrapper">
+      <BookedTimes
+        className="book-times-icon"
+        viewBox="0 0 24 24"
+        fill="#333333"
+      />
+      <div className="restaurant-booking-stat-text">
+            Booked
+        {' '}
+        {bookingCount}
+        {' '}
+            times today
       </div>
-    );
-  }
-}
+    </div>
+    <div className="book-stat-wrapper">
+      <ActFast
+        className="act-fast-icon"
+        viewBox="0 0 24 24"
+        fill="#333333"
+      />
+      <div className="restaurant-booking-stat-text">
+        {'You\'re in luck! We still have'}
+        {' '}
+        {availableTargetTimeSlots.length}
+        {' '}
+            timeslots left
+      </div>
+    </div>
+  </div>
+);
 
 BookingStat.propTypes = {
-  availableDateTimeSlots: propTypes.number.isRequired,
+  availableTargetTimeSlots: propTypes.array.isRequired,
+  bookingCount: propTypes.number.isRequired,
 };
 
 export default BookingStat;
