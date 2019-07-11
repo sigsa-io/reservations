2c982825915fb92fa469e8bab3ba029ae5f9775e
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-10 17:56:02 -0700

    serverside get seatingsize method implemented targetTime updated

--- server/Route.js
+++ server/Route.js
@@ -1,43 +1,46 @@
 const express = require('express');
+const moment = require('moment');
 const db = require('./db/index');
 
 const router = express.Router();
 
-router.get('/targettimeslots/:restaurant_id', (req, res) => {
-  const { restaurant_id } = req.params;
-  const { targetTimeUnix, userPartySize } = req.query;
+// front end will first call this to evaluate if the party size is too big
+router.get('/seatingSize/:restaurantId', (req, res) => {
+  const { restaurantId } = req.params;
 
-  const timeLowerBound = Number(targetTimeUnix) - 60 * 60 * 2.5;
-  const timeUpperBound = Number(targetTimeUnix) + 60 * 60 * 2.5;
-  const partySize = Number(userPartySize);
-
-  // need to evaluate if partysize fit
-  // need to get 2.5 hr range (both before and after, thus 5hr range in total)
-  const queryStr =
-    'SELECT * FROM reservations_tables WHERE restaurant_id = ? ' +
-    'AND availableSeats > ? AND (reservationTimeStamp BETWEEN ? AND ?)';
-  const queryArg = [restaurant_id, partySize, timeLowerBound, timeUpperBound];
-
-  return db.query(queryStr, queryArg, (err, data) => {
+  const queryStr = 'SELECT availableSeats FROM restaurants WHERE restaurantId = ? LIMIT 1';
+  
+  return db.query(queryStr, restaurantId, (err, data) => {
     if (err) {
       res.status(500).json(err);
     } else {
+      console.log(data);
       res.status(200).json(data);
     }
   });
 });
 
-// get available time slots for target date
-router.get('/datetimeslots/:restaurantId', (req, res) => {
+// after validating the partysize fit, then we find the available seating slots
+router.get('/targettimeslots/:restaurantId', (req, res) => {
   const { restaurantId } = req.params;
-  const { targetDateStartUnix } = req.body;
-  const targetDateEndUnix = Number(targetDateStartUnix) + 60 * 60 * 24;
+  const { targetTimeUnix, userPartySize } = req.body;
+
+  const timeLowerBound = Number(targetTimeUnix) - 60 * 60 * 2.5;
+  const timeUpperBound = Number(targetTimeUnix) + 60 * 60 * 2.5;
+
+  const targetTimeStr = moment.unix(targetTimeUnix).format('HH:mm');
+  const targetTimeNum = Number(targetTimeStr.split(':')[0]) + Number(targetTimeStr.split(':')[1]) / 60;
+  const targetTimeLowerBoundNum = targetTimeNum - 2.5;
+  const targetTimeUpperBoundNum = targetTimeNum + 2.5;
+
+  const partySize = Number(userPartySize);
 
   // find reservations within this unix range, partysize doesn't matter
   const queryStrReservations =
     'SELECT * FROM reservations WHERE restaurantId = ? ' +
-    'AND (start_time BETWEEN ? AND ?)';
-  const queryArgReservations = [restaurantId, targetDateStartUnix, targetDateEndUnix];
+    'AND start_time >= ? ' + 
+    'AND end_time <= ?';
+  const queryArgReservations = [restaurantId, timeLowerBound, timeUpperBound];
   return db.query(queryStrReservations, queryArgReservations, (err, data) => {
     if (err) {
       res.status(500).json(err);
@@ -55,30 +58,42 @@ router.get('/datetimeslots/:restaurantId', (req, res) => {
           if (err) {
             res.status(500).json(err);
           } else {
-            console.log(data);
             res.status(200).json(data);
           }
         })
       } else {
-        // we first get the reservation in 'AMPM' format but in number
-
+        // we first get the reservation start_time in 'AMPM' format but in number
+        const startTimeInReservation = {};
+        for (var i = 0; i < data.length; i ++) {
+          const timeSlotStr = moment.unix(data[i].start_time).format('HH:mm');
+          const timeSlotNum = Number(timeSlotStr.split(':')[0]) + Number(timeSlotStr.split(':')[1]) / 60;
+          startTimeInReservation[timeSlotNum] = startTimeInReservation[timeSlotNum] + data[i].partySize || data[i].partySize;
+        }
+        // find within bound time slot from restaurant table
+        queryStrRestaurant =
+          'SELECT * FROM restaurants WHERE restaurantId = ?' +
+          'AND (timeSlot BETWEEN ? AND ?)';
+        queryArgRestaurant = [ restaurantId, targetTimeLowerBoundNum, targetTimeUpperBoundNum ];
+        db.query(queryStrRestaurant, queryArgRestaurant, (err, data) => {
+          if (err) {
+            res.status(500).json(err);
+          } else {
+            // evaluate if the time slot is full for reservation such party size
+            const availableTimeSlot = [];
+            for (var j = 0; j < data.length; j++) {
+              if (startTimeInReservation[data[j].timeSlot]) {
+                if (data[j].availableSeats - startTimeInReservation[data[j].timeSlot] > partySize) {
+                  availableTimeSlot.push(data[j]);
+                }
+              } else {
+                availableTimeSlot.push(data[j]);
+              }
+            }
+            console.log(availableTimeSlot);
+            res.status(200).json(availableTimeSlot);
+          }
+        });
       }
-    }
-  })
-
-
-
-
-  const queryStr =
-    'SELECT * FROM reservations_tables WHERE restaurant_id = ? ' +
-    'AND (reservationTimeStamp BETWEEN ? AND ?)';
-  const queryArg = [restaurant_id, targetDateStartUnix, targetDateEndUnix];
-
-  return db.query(queryStr, queryArg, (err, data) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(200).json(data);
     }
   });
 });
@@ -90,7 +105,8 @@ router.post('/reservations', (req, res) => {
 
   const queryStrReservation = 'INSERT INTO reservations (restaurantId, start_time, end_time, partySize) VALUES (?, ?, ?, ?)';
   const queryArgReservation = [restaurantId, targetStartTimeUnix, targetEndTimeUnix, partySize];
-  const queryStrBookingCount = 'INSERT INTO bookingCount (restaurantId, bookingCount) VALUES (?, 1) ON DUPLICATE KEY UPDATE bookingCount = VALUES(bookingCount) + 1';
+  // const queryStrBookingCount = 'INSERT INTO bookingCount (restaurantId, bookingCount) VALUES (?, 1) ON DUPLICATE KEY UPDATE bookingCount = VALUES(bookingCount) + 1';
+  const queryStrBookingCount = 'UPDATE bookingCount SET bookingCount = bookingCount + 1 WHERE restaurantId = ?';
   const queryArgBookingCount = [restaurantId];
 
   return db.query(queryStrReservation, queryArgReservation, (err, data) => {
