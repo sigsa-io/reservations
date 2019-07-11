b949d5abb307d6c82f901374ad5a007e3c300064
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-11 00:27:10 -0700

    booking inluck message conditionally render

--- client/components/BookingStat.jsx
+++ client/components/BookingStat.jsx
@@ -3,38 +3,50 @@ import propTypes from 'prop-types';
 import BookedTimes from '../img/BookTimes';
 import ActFast from '../img/ActFast';
 
-const BookingStat = ({ bookingCount, availableTargetTimeSlots }) => (
-  <div className="book-stat">
-    <div className="book-stat-wrapper">
-      <BookedTimes
-        className="book-times-icon"
-        viewBox="0 0 24 24"
-        fill="#333333"
-      />
-      <div className="restaurant-booking-stat-text">
-            Booked
-        {' '}
-        {bookingCount}
-        {' '}
-            times today
+const BookingStat = ({ bookingCount, availableTargetTimeSlots, inLuckView }) => {
+  const renderInLuckMsg = () => {
+    if (inLuckView) {
+      return (
+        <div className="book-stat-wrapper">
+          <ActFast
+            className="act-fast-icon"
+            viewBox="0 0 24 24"
+            fill="#333333"
+          />
+          <div className="restaurant-booking-stat-text">
+            {'You\'re in luck! We still have'}
+            {' '}
+            {availableTargetTimeSlots.length}
+            {' '}
+                timeslots left
+          </div>
+        </div>
+      );
+    }
+
+    return <div></div>;
+  }
+
+  return (
+    <div className="book-stat">
+      <div className="book-stat-wrapper">
+        <BookedTimes
+          className="book-times-icon"
+          viewBox="0 0 24 24"
+          fill="#333333"
+        />
+        <div className="restaurant-booking-stat-text">
+              Booked
+          {' '}
+          {bookingCount}
+          {' '}
+              times today
+        </div>
       </div>
+      { renderInLuckMsg() }
     </div>
-    <div className="book-stat-wrapper">
-      <ActFast
-        className="act-fast-icon"
-        viewBox="0 0 24 24"
-        fill="#333333"
-      />
-      <div className="restaurant-booking-stat-text">
-        {'You\'re in luck! We still have'}
-        {' '}
-        {availableTargetTimeSlots.length}
-        {' '}
-            timeslots left
-      </div>
-    </div>
-  </div>
-);
+  );
+}
 
 BookingStat.propTypes = {
   availableTargetTimeSlots: propTypes.array.isRequired,
