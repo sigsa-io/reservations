102fa946639dd9c43a3af36f4229bec293021988
Jennie Zeng <jenniezeng1@gmail.com>
2019-07-10 23:44:16 -0700

    maxparty warning render with dynamic restaurant name

--- client/components/MaxPartySize.jsx
+++ client/components/MaxPartySize.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import SVG from '../img/Warning';
 
-const MaxPartySize = () => (
+const MaxPartySize = ({ restaurantName }) => (
     <span className="no-time-slot-wrapper">
       <div className="no-time-slot">
         <div className="no-time-slot-text-wrap">
@@ -10,11 +11,15 @@ const MaxPartySize = () => (
             viewBox="0 0 1000 1000"
             fill="333333"
           />
-            {`Unfortunately, your party is too large to make an online reservation at Cala. 
+            {`Unfortunately, your party is too large to make an online reservation at ${restaurantName}. 
             We recommend contacting the restaurant directly.`}
         </div>
       </div>
     </span>
   );
+
+  MaxPartySize.propTypes = {
+    restaurantName: propTypes.string.isRequired,
+  }
   
   export default MaxPartySize;
